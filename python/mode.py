1dd4853101c87479421fc1974442656f630bb03b
beomwhale <ephesi4ns623@gmail.com>
2023-06-17 23:39:31 +0900

    greedy

--- python/mode.py
+++
@@ -1,9 +0,0 @@
-def solution(array):
-    while len(array) != 0:
-        for i, a in enumerate(set(array)):
-            print(f"{i} : {a}")
-            array.remove(a)
-        if i == 0: return a
-    return -1
-
-print(solution([1,2,3,3,3,4,4]))
