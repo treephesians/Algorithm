6910ddeb6f70472ddf002e21254eeb8cc37ca583
ephesi4ns <ephesi4ns623@gmail.com>
2024-03-25 13:46:36 +0900

    Daily PS

--- boj/divide-conquer/2630.py
+++ boj/divide-conquer/2630.py
@@ -15,6 +15,7 @@ def is_all_white(row, col, n, matrix):
     return 1
 
 def divide_and_conquer(row, col, n, matrix, n_blue, n_white):
+    
     if is_all_blue(row, col, n, matrix):
         n_blue += 1
     elif is_all_white(row, col, n, matrix):
@@ -27,22 +28,17 @@ def divide_and_conquer(row, col, n, matrix, n_blue, n_white):
         n_blue, n_white = divide_and_conquer(row + half_n, col, half_n, matrix, n_blue, n_white)
         n_blue, n_white = divide_and_conquer(row + half_n, col + half_n, half_n, matrix, n_blue, n_white)
     return n_blue, n_white
+    
 
-# Assuming N and matrix are given as input in the correct format
-# For demonstration, replace the sys.stdin.readline() with some fixed values
-# N = int(input())
-# matrix = []
-# for i in range(N):
-#     row = list(map(int, input().split()))
-#     matrix.append(row)
+N = int(sys.stdin.readline())
+matrix = []
 
-n_blue = 0
+for i in range(N):
+    row = list(map(int, sys.stdin.readline().split()))
+    matrix.append(row)
+
 n_white = 0
-
-# Example input, replace with the actual input mechanism
-N = 8
-matrix = [[1, 1, 0, 0, 0, 0, 1, 1], [1, 1, 0, 0, 0, 0, 1, 1], [0, 0, 0, 0, 1, 1, 0, 0], [0, 0, 0, 0, 1, 1, 0, 0], [1, 0, 0, 0, 1, 1, 1, 1], [0, 1, 0, 0, 1, 1, 1, 1], [0, 0, 1, 1, 1, 1, 1, 1], [0, 0, 1, 1, 1, 1, 1, 1]]
-print(matrix)
+n_blue = 0
 n_blue, n_white = divide_and_conquer(0, 0, N, matrix, n_blue, n_white)
 
 print(n_white)
