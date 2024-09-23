6a1e3926fbb353a7ffa4a70c32115cbcd3ee861f
Junbeom Park <ephesi4ns623@gmail.com>
2024-09-23 11:25:00 +0900

    1967:tree, dfs

--- boj/tree/1967.py
+++ boj/tree/1967.py
@@ -6,20 +6,19 @@ def find_max_len_v(n, start, tree):
     max_len = 0
     max_v = start
     while stack:
-        v = stack.pop()[0]
-        w = stack.pop()[1]
+        now = stack.pop()
+        v = now[0]
+        w = now[1]
         visited[v] = True
-        if len(tree[v]) > 1:
-            for i in range(len(tree[v])):
-                next_v = tree[v][i][0]
-                next_w = tree[v][i][1]
-                if not visited[next_v]:
-                    stack.append([next_v, w + next_w])
-        else:
-            if max_len < w:
-                max_len = w
-                max_v = v
-    return max_v
+        for i in range(len(tree[v])):
+            next_v = tree[v][i][0]
+            next_w = tree[v][i][1]
+            if not visited[next_v]:
+                stack.append([next_v, w + next_w])
+        if max_len < w:
+            max_len = w
+            max_v = v
+    return max_v, max_len
 
 
 n = int(sys.stdin.readline())
@@ -31,5 +30,7 @@ for _ in range(n - 1):
     tree[p].append([c, w])
     tree[c].append([p, w])
 
-v = find_max_len_v(n, 1, tree)
-print(v)
+v, l = find_max_len_v(n, 1, tree)
+result_v, result_l = find_max_len_v(n, v, tree)
+
+print(result_l)
