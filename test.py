0fd89d07ef2ee411b9cfbc87b4b5846a7dc15de4
Junbeom Park <ephesi4ns623@gmail.com>
2025-01-03 23:46:52 +0900

    Graph S2

--- test.py
+++ test.py
@@ -1,33 +1,29 @@
 import sys
+from collections import deque
 
 input = sys.stdin.readline
 
 N = int(input())
-# o o o o
-# o o o o
-# o o o o
-# o o o o
-count = 0
-col_arr = [True for _ in range(N)]
-plus_diagonal = [True for _ in range(2 * N - 1)]
-minus_diagonal = [True for _ in range(2 * N - 1)]
 
-def nQueens(row, col, col_arr, plus_diagonal, minus_diagonal):
-    global count
-    if row == N - 1:
-        count += 1
-        return
-    col_arr[col] = False
-    plus_diagonal[row + col] = False
-    minus_diagonal[row - col] = False
-    for i in range(N):
-        if col_arr[i] and plus_diagonal[row + 1 + i] and minus_diagonal[row + 1 - i]:
-            nQueens(row + 1, i, col_arr, plus_diagonal, minus_diagonal)
-    col_arr[col] = True
-    plus_diagonal[row + col] = True
-    minus_diagonal[row - col] = True
+graph = [[] for _ in range(N + 1)]
+visited = [False for _ in range(N + 1)]
+result = [0 for _ in range(N + 1)]
+for _ in range(N - 1):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
 
-for col in range(N):
-    nQueens(0, col, col_arr, plus_diagonal, minus_diagonal)
+q = deque([1])
 
-print(count)
+while q:
+    now = q.popleft()
+    if not visited[now]:
+        visited[now] = True
+        for next in graph[now]:
+            if not visited[next]:
+                result[next] = now
+                q.append(next)
+
+for i in range(2, N + 1):
+    print(result[i])
+
