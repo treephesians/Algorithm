00c9d2c4725e04fd820a11210a801edaefc8fd62
Junbeom Park <ephesi4ns623@gmail.com>
2025-01-10 12:45:06 +0900

    Fix Math G4

--- boj/math/27172.py
+++ boj/math/27172.py
@@ -5,23 +5,20 @@ N = int(input())
 cards = list(map(int, input().split()))
 
 # 각 카드의 점수 초기화
-scores = {card:0 for card in cards}
+scores = {card: 0 for card in cards}
 
 # 카드 정렬
 cards_sorted = sorted(cards)
+cards_set = set(cards)  # 빠른 탐색을 위해 set 생성
 
 # 카드별 점수 계산 (배수 관계 탐색)
-max_card = cards_sorted[-1]
-
-for i in range(N):
-    x = cards_sorted[i]
+for x in cards_sorted:
     multiple = x * 2
-    while multiple <= max_card:
-        # 배수 관계에 해당하는 카드의 점수 조정
-        if multiple in cards:
+    while multiple <= cards_sorted[-1]:
+        if multiple in cards_set:  # set에서 O(1)로 탐색
             scores[x] += 1  # x가 승리
             scores[multiple] -= 1  # multiple이 패배
         multiple += x
 
 # 결과 출력
-print(" ".join(map(str, scores.values())))
+print(" ".join(map(str, [scores[card] for card in cards])))
