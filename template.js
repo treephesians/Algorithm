41a6d8d995b0f5b9d6bdf7a4d465c53998da5bba
Junbeom Park <ephesi4ns623@gmail.com>
2025-02-12 20:34:26 +0900

    something

--- template.js
+++ template.js
@@ -1,25 +1,5 @@
-const readline = require("readline");
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+//const input = fs.readFileSync("../input.txt").toString().trim().split("\n");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-let T = 1; // TestCase 여러 개이면 여기를 수정해야한다.
-const inputs = [];
-let count = 0;
-
-rl.on("line", (line) => {
-  if (T === undefined) {
-    T = parseInt(line.trim(), 10);
-  } else {
-    inputs.push(line.trim());
-    count++;
-
-    if (count === T) {
-      rl.close();
-    }
-  }
-});
-
-rl.on("close", () => {});
+const n = parseInt(input[0]); // 도시의 개수
