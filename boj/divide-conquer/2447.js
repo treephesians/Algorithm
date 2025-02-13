8427aeac22019f9f71018d1e6bbf63a1727ff1e9
Junbeom Park <ephesi4ns623@gmail.com>
2025-02-13 14:53:09 +0900

    (G5) Divide Conquer

--- boj/divide-conquer/2447.js
+++ boj/divide-conquer/2447.js
@@ -5,4 +5,26 @@ const N = parseInt(input[0])
 
 const matrix = Array.from({length:N}, ()=>Array(N).fill("*"))
 
-console.log(matrix)
+function O(N, row, col){
+    if (N == 1) return;
+    const size = N / 3;
+    for (let i = row + size; i < row + 2 * size; i++){
+        for (let j = col + size; j < col + 2 * size; j++){
+            matrix[i][j] = " ";
+        }
+    }
+    O(N / 3, row, col);
+    O(N / 3, row + size, col);
+    O(N / 3, row + 2 * size, col);
+    O(N / 3, row, col + size);
+    O(N / 3, row, col + 2 * size);
+    O(N / 3, row + size, col + 2 * size);
+    O(N / 3, row + 2 * size, col + size);
+    O(N / 3, row + 2 * size, col + 2 * size);
+}
+
+O(N, 0, 0);
+
+for (let row = 0; row < N; row++){
+    console.log(matrix[row].join(""));
+}
