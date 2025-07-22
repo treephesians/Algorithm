798f1dedab6abe78d98a12752573e875934a76ed
Junbeom Park <ephesi4ns623@gmail.com>
2025-07-22 10:13:02 +0900

    S2: DFS

--- test.py
+++ test.py
@@ -3,14 +3,29 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-A = list(map(int, input().split()))
 
-indexes = [1] * N
-for i in range(1, N):
-    max_index = 0
-    for j in range(0, i):
-        if A[i] > A[j]:
-            max_index = max(max_index, indexes[j])
-        indexes[i] = max_index + 1
+graph = [[] for _ in range(N + 1)]
+visited = [False] * (N + 1)
+arr = [0] * (N + 1)
 
-print(max(indexes))
+for _ in range(N - 1):
+    v1, v2 = map(int, input().split())
+    graph[v1].append(v2)
+    graph[v2].append(v1)
+
+def DFS_stack(start):
+    stack = [start]
+    visited[start] = True
+
+    while stack:
+        parent = stack.pop()
+        for child in graph[parent]:
+            if not visited[child]:
+                visited[child] = True
+                arr[child] = parent
+                stack.append(child)
+
+DFS_stack(1)
+
+for i in range(2, N + 1):
+    print(arr[i])
